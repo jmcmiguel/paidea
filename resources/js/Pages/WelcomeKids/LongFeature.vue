8a26185b70cab93b8d19c908330bc8166098be20
poodzia <poodzia@gmail.com>
2022-03-13 04:58:19 +0800

    Margin Fixes

--- resources/js/Pages/WelcomeKids/LongFeature.vue
+++ resources/js/Pages/WelcomeKids/LongFeature.vue
@@ -2,15 +2,15 @@
     <div class="flex flex-col bg-[#f5f5f5] lg:bg-[url('/img/highlight1.png')] min-h-screen bg-cover">
         <h1 class="text-center lg:text-right lg:pr-32 pt-32 text-5xl text-[#707070] font-bold tracking-wide">Learn More With <span class="text-[#f7941d]"> Online Paideia </span> </h1>
     
-        <div class="text-lg w-[80vw] lg:w-[30vw] font-medium lg:ml-[60vw] text-[#707070] mt-32 flex flex-col space-y-10 text-justify">
+        <div class="text-lg w-[80vw] lg:w-[30vw] 2xl:pb-[50vh] font-medium lg:ml-[60vw] text-[#707070] mt-32 flex flex-col space-y-10 text-justify">
             <span class="flex"> <i class="material-icons text-[#f7941d]" style="font-size:4rem;">arrow_right</i> <p> Your child may attend the tutoring sessions from the comfort of your own home. In our online integrated platform, children may collaborate and communicate with their Arithmetic tutor to solve math problems more effectively. </p> </span>
             <span class="flex"> <i class="material-icons text-[#f7941d]" style="font-size:4rem;">arrow_right</i> <p>Your child and their Math Tutor will continue to work together to learn and build on the foundations of each lesson. You can schedule classes directly with your Math Tutor as required. There is no need to make a commitment.</p> </span>
         </div>
     </div>
 
-    <div class="lg:bg-[url('/img/highlight2.png')] min-h-screen bg-cover bg-right">
+    <div class="lg:bg-[url('/img/highlight2.png')] min-h-screen bg-cover bg-top-right">
 
-        <h1 class="text-[#f7941d] lg:text-white text-4xl text-center font-bold pr-0 lg:pr-64 pt-12 pb-6 lg:pb-3 lg:pt-56">Your Child Success is <br/> Our No.1 Priority</h1>
+        <h1 class="text-[#f7941d] lg:text-white text-4xl text-center font-bold pr-0 pt-12 pb-6 lg:pb-3 lg:pt-56">Your Child Success is <br/> Our No.1 Priority</h1>
 
         <div class="flex flex-col space-y-16">
             <div class="drop-shadow-2xl hover:scale-125 transition-all ease-in-out cursor-pointer w-fit tracking-wider text-[#757575] bg-[#ffffff] mx-auto lg:ml-36 py-3 px-5 text-xl lg:text-2xl font-semibold rounded-full leading-loosefont-semibold" >
